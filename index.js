6e5811ae8f9ed99a211681f84c9144e04aa500b8
audiantaslim-git <audiantaslim.work@gmail.com>
2025-07-18 16:36:43 +0700

    First Commit Base Files

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import axios from 'axios';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
